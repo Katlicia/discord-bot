250754e84dc5f1e9bcc9ee61350bfaaa732075cc
Katlicia <mama-4153@hotmail.com>
2024-09-21 20:46:05 +0300

    log system for admin

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -6,6 +6,18 @@ class SlashCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def ensure_log_channel(self, guild: discord.Guild):
+        # Checks channel named Alo-log if doesn2t exit creates one.
+        log_channel = discord.utils.get(guild.text_channels, name="alo-log")
+        if log_channel is None:
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(view_channel = False),
+                guild.default_role: discord.PermissionOverwrite(read_messages=True, send_messages=False),
+                guild.me: discord.PermissionOverwrite(send_messages=True)
+            }
+            log_channel = await guild.create_text_channel("alo-log", overwrites=overwrites)
+        return log_channel
+
     # Bans User
     @app_commands.command(name="ban", description="Ban User")
     @app_commands.checks.has_permissions(ban_members=True)
@@ -13,6 +25,9 @@ class SlashCommands(commands.Cog):
         try:
             await member.ban(reason=reason)
             await interaction.response.send_message(f"{member.name} is banned. Reason: {reason}", ephemeral=True, delete_after=5)
+            # Saves the process in alo-log.
+            log_channel = await self.ensure_log_channel(interaction.guild)
+            await log_channel.send(f"{interaction.user.name} banned {member.name}. Reason: {reason}")
         except Exception as e:
             await interaction.response.send_message(f"An error happened: {str(e)}", ephemeral=True, delete_after=5)
 
@@ -30,15 +45,18 @@ class SlashCommands(commands.Cog):
         await interaction.response.defer(ephemeral=True)
         if amount > 0:
             deleted = await interaction.channel.purge(limit=amount)
-            await interaction.followup.send(f"{len(deleted)} messages deleted successfully.", ephemeral=True)
+            await interaction.followup.send(f"{len(deleted)} messages deleted successfully.", ephemeral=True, delete_after=5)
+            # Saves the process in alo-log
+            log_channel = await self.ensure_log_channel(interaction.guild)
+            await log_channel.send(f"{interaction.user.name} deleted {len(deleted)} messages in {interaction.channel.name}.")
         else:
-            await interaction.followup.send("Invalid number.", ephemeral=True)
+            await interaction.followup.send("Invalid number.", ephemeral=True, delete_after=5)
 
     # If user doesn't have the permission to remove messages show error to only user.
     @temizle.error
     async def temizle_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
         if isinstance(error, app_commands.CheckFailure):
-            await interaction.followup.send("You can't use this command.", ephemeral=True)
+            await interaction.followup.send("You can't use this command.", ephemeral=True, delete_after=5)
 
 async def setup(bot):
     await bot.add_cog(SlashCommands(bot))
