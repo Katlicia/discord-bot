50c15541667d2e501db5be3b92d653063a2ace86
Katlicia <yusuful80@gmail.com>
2025-12-19 18:19:38 +0300

    removed unnecesary auto commands

--- bot.py
+++ bot.py
@@ -16,27 +16,10 @@ async def on_ready():
     print(f"Bot signed in as: {bot.user.name}")
     await bot.tree.sync()
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if check_keyword(message.content, "31"):
-        await message.channel.send("QWPEOIRTPOWEIORHOPOIKWRETPOLIHJKWRTLŞHGKWERFPOĞGWERF"),
-    if check_keyword(message.content, "ibo"):
-        await message.reply(":sob: :skull:")
-    if check_keyword(message.content, "41"):
-        await message.reply(":flag_tr:")
-    if check_keyword(message.content, "17"):
-        await message.reply(":flag_tr:")
-    if message.content.lower() == "sa":
-        await message.channel.send("as")
-    await bot.process_commands(message)
-
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
-
 
 async def main():
     async with bot:
